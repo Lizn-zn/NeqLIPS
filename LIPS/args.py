3720f9e1abf2490f8ef6f99dcd50eee5f4a0c62e
lizenan <lizenan1995@foxmail.com>
2025-02-19 21:52:03 +0800

    update

--- LIPS/args.py
+++ LIPS/args.py
@@ -17,15 +17,15 @@ def parse_args():
     argparser.add_argument("--check_cycle", default=0, choices=[0,1], type=int, help="Whether using the cycle check")
     argparser.add_argument("--check_homogeneous", default=1, choices=[0, 1], type=int, help="Whether to make the problem homogeneous previously")
     # Ranker params
-    argparser.add_argument("--focus_ops", default="d+", type=str, help="Operator encouraged for the problem, e.g., d+")
-    argparser.add_argument("--norm_goal", default=0, choices=[0,1], type=int, help="Whether to normalize the goal")
+    argparser.add_argument("--focus_ops", default="dm", type=str, help="Operator encouraged for the problem, e.g., `dm` means decoupling and homogeneity")
+    argparser.add_argument("--norm_goal", default=0, choices=[0,1], type=int, help="Whether to normalize the goal before ranking")
     argparser.add_argument("--rank_size", default=10, type=int, help="Filter size for the Ranker")
     # Scaler params
     argparser.add_argument("--scale_limit", default=128, type=int, help="Size limit for each pattern match of given premise, -1 means unlimited")
     argparser.add_argument("--scale_equality", default=0, choices=[0,1], type=int, help="Whether using the equality check")
-    argparser.add_argument("--init_test", default="auto", type=str, help="Values dictionary as a string or auto")
+    argparser.add_argument("--init_test", default="auto", type=str, help="Test cases for the problem, values dictionary as a string or auto")
     # Rewriter params
-    argparser.add_argument("--rewrite_limit", default=1, type=int, help="Number of candidates when llm rewriting")
+    argparser.add_argument("--rewrite_limit", default=1, type=int, help="Number of LLM generatedcandidates when llm rewriting")
     argparser.add_argument("--rewrite_length", default=5, type=int, help="Maximum subsequent rewrite steps for the problem")
     argparser.add_argument("--sym_rewrite", default=0, choices=[0,1], type=int, help="Whether only using the symbolic rewrite")
     # params for the smt solver
